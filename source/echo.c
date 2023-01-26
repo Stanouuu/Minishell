78aa22b65893c8611aaf85fe3f0ab39199c72dbd
Stanislas BARRAGE <sbarrage@e1r4p3.clusters.42paris.fr>
2023-01-26 18:32:44 +0100

    des touch rqjouter a echo

--- source/echo.c
+++ source/echo.c
@@ -6,18 +6,42 @@
 /*   By: sbarrage <sbarrage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 16:18:50 by sbarrage          #+#    #+#             */
-/*   Updated: 2023/01/26 16:49:22 by sbarrage         ###   ########.fr       */
+/*   Updated: 2023/01/26 18:28:56 by sbarrage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// void	ft_printstr_mod(char *str)
+// {
+// 	int	i;
+// 	char c;
+
+// 	while (str[i])
+// 	{
+// 		// if (str[i] == '"' )
+// 		// {
+// 		// 	write(1, "a", 1);
+// 		// 	while (str[i] != '"')
+// 		// 		write(1, &str[i++], 1);
+// 		// }
+
+// 		write(1, &str[i], 1);
+// 		i++;
+// 		// else if (str[i] == '\'' )
+// 		// {
+// 		// 	while (str[i] != '\'')
+// 		// 		write(1, &str[i++], 1);
+// 		// }
+// 	}
+// }
 
 void	echo(char *rd)
 {
 	int	i;
 	int	rall;
 
-	rall = 1;
+	rall = 0;
 	i = 0;
 	while (ft_isalpha(rd[i]) == 1)
 	{
@@ -25,17 +49,19 @@ void	echo(char *rd)
 	}
 	while (rd[i] == ' ')
 		i++;
-	// printf("%d, %d\n", i, ft_strncmp("-n ", rd + i, 3));
-	// || strncmp("-n", rd + i, 3) == 0)
-	while ((strncmp("-n ", rd + i, 3) == 0 || strncmp("-n", rd + i, 3) == 0) && rd[i])
+	while ((strncmp("-n", rd + i, 2) == 0) && rd[i])
 	{
-		rall = 0;
-		i += 2;
+		i++;
+		while (rd[i] == 'n')
+			i++;
+		if (rd[i] != '\0' && rd[i] != ' ')
+			break;
 		while (rd[i] == ' ')
 			i++;
+		rall = i;
 	}
-	if (rall == 1)
+	if (rall == 0)
 		printf("%s\n", rd + i);
-	else 
+	else
 		printf("%s", rd + i);
 }
