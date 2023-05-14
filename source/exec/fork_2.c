094e3d6752b2806d638c67b4298fd425e768acc9
Stanouuu <stanislas.barrage@gmail.com>
2023-05-14 16:44:44 +0200

    all work

--- source/exec/fork_2.c
+++ source/exec/fork_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fork_2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gfranque <gfranque@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sbarrage <sbarrage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:55:12 by sbarrage          #+#    #+#             */
-/*   Updated: 2023/05/14 13:45:09 by gfranque         ###   ########.fr       */
+/*   Updated: 2023/05/14 16:24:47 by sbarrage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	comewithmeifyouwanttofork(int *pid, int y, t_data *data, char **str)
 {
 	if (pid[y] < 0)
 		ft_error("fork");
-	// if (data->next)
-	// 	close(data->fd[1]);
 	(void)data;
 	if (*str)
 		free(*str);
@@ -33,12 +31,10 @@ int	the_fork_outa_names(t_data *data, int x, int j)
 	return (i);
 }
 
-t_data	*next_data(t_data *data)
+t_data	*next_data(t_data *data, int x)
 {
-	// close(data->fd[0]);
-	// close(data->fd[1]);
 	if (data->index == 0)
-		firstcommandclose(data, 0);
+		firstcommandclose(data, 0, x);
 	else if (data->next == NULL && data->index != 0)
 		lastcommandclose(data, 0);
 	else
@@ -48,6 +44,7 @@ t_data	*next_data(t_data *data)
 
 void	forkgive_n_forkget(t_data *data, int *pid, int *j, char **str)
 {
+	ft_printf("J : %d - %d\n", j[0], j[1]);
 	ft_printf("I : %d - %d\n", data->fd[0], data->fd[1]);
 	ft_printf("pipe : %d - %d\n", data->pipe[0], data->pipe[1]);
 	forkland(data, pid, j);
--- source/parsing/controll.c
+++ source/parsing/controll.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   controll.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gfranque <gfranque@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sbarrage <sbarrage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:11:06 by sbarrage          #+#    #+#             */
-/*   Updated: 2023/05/13 22:38:17 by gfranque         ###   ########.fr       */
+/*   Updated: 2023/05/14 16:21:03 by sbarrage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,5 @@ int	road(t_data *data)
 {
 	if (ft_command(data) == -1)
 		return (-1);
-	// while (data)
-	// {
-	// 	close(data->fd[0]);
-	// 	data = data->next;
-	// }
 	return (1);
 }
