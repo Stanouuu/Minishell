05f9cbd345e1233464b1e384da1365a7cb7d1e6a
Stanislas Barrage <stanislas.barrage@gmail.com>
2024-01-19 22:36:55 +0100

    cleaning up

--- source/exec/commandclose.c
+++ source/exec/commandclose.c
@@ -6,7 +6,7 @@
 /*   By: sbarrage <sbarrage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 13:16:14 by gfranque          #+#    #+#             */
-/*   Updated: 2023/05/15 20:21:42 by sbarrage         ###   ########.fr       */
+/*   Updated: 2023/05/16 16:19:11 by sbarrage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,48 @@
 
 void	firstcommandclose(t_data *data, int n, int x)
 {
-	if (data->fd[0] != x)
+	if (data->fd[0] != -1 && data->fd[0] != x)
 		close(data->fd[0]);
 	if (data->next)
 	{
-		if (n == 1)
+		if (n == 1 && data->next->pipe[0] != -1)
 			close(data->next->pipe[0]);
-		if (data->next->pipe[1] != data->fd[1])
+		if (data->next->pipe[1] != -1
+			&& data->next->pipe[1] != data->fd[1])
 			close(data->next->pipe[1]);
 	}
-	close(data->fd[1]);
+	if (data->fd[1] != -1)
+		close(data->fd[1]);
 }
 
 void	lastcommandclose(t_data *data, int n)
 {
 	(void)n;
-	close(data->fd[1]);
-	close(data->pipe[1]);
-	if (data->pipe[0] != data->fd[0])
+	if (data->fd[1] != -1)
+		close(data->fd[1]);
+	if (data->pipe[1] != -1)
+		close(data->pipe[1]);
+	if (data->pipe[0] != data->fd[0]
+		&& data->pipe[0] != -1)
 		close(data->pipe[0]);
-	close(data->fd[0]);
+	if (data->fd[0] != -1)
+		close(data->fd[0]);
 }
 
 void	middlecommandclose(t_data *data, int n)
 {
-	close(data->pipe[1]);
-	if (n == 1)
+	if (data->pipe[1] != -1)
+		close(data->pipe[1]);
+	if (n == 1 && data->next->pipe[0] != -1)
 		close(data->next->pipe[0]);
-	if (data->pipe[0] != data->fd[0])
+	if (data->pipe[0] != data->fd[0]
+		&& data->pipe[0] != -1)
 		close(data->pipe[0]);
-	if (data->next->pipe[1] != data->fd[1])
+	if (data->next->pipe[1] != data->fd[1]
+		&& data->next->pipe[1] != -1)
 		close(data->next->pipe[1]);
-	close(data->fd[0]);
-	close(data->fd[1]);
+	if (data->fd[0] != -1)
+		close(data->fd[0]);
+	if (data->fd[1] != -1)
+		close(data->fd[1]);
 }
